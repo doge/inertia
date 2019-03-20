a4bee95fa56cd4987fc532547e5cdd27c6b4d9cb
Gabe <16624137+pain@users.noreply.github.com>
2019-03-20 12:20:23 -0400

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -6,7 +6,8 @@ def hello():
 def world():
     print("World")
 
+inertia.title("My Menu")
 inertia.add_item("hello", hello)
 inertia.add_item("world", world)
 inertia.add_item("hello world", 'print("hello world")')
-inertia.display()
+inertia.render()
