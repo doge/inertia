e405233bc47847eb50bb7c93d9cb500586a5a96f
Gabe <16624137+pain@users.noreply.github.com>
2019-03-20 14:15:12 -0400

    added colors

--- lib/inertia_repl.py
+++ lib/inertia_repl.py
@@ -1,11 +1,27 @@
-import getch, os, replit
+import getch, replit
 
-current = 0
 items = {}
+current = 0
 title_text = ""
+
+current_color = ""
+colors = {
+  "black": "\033[1;30;1m",
+  "red": "\033[1;31;1m",
+  "green": "\033[1;32;1m",
+  "yellow": "\033[1;33;1m",
+  "blue": "\033[1;34;1m",
+  "purple": "\033[1;35;1m",
+  "cyan": "\033[1;36;1m",
+  "white": "\033[1;37;1m"
+}
 
 def separator():
   print("-----------------------")
+
+def color(col):
+  global current_color
+  current_color = col
 
 def title(text):
   global title_text
@@ -14,23 +30,26 @@ def title(text):
 def add_item(name, function):
   items[name] = function
 
+if current_color == "":
+  current_color = "white"
+
 def populate():
   if len(title_text) > 0:
-    print(title_text)
+    print(colors["white"] + title_text)
   for x in range(0, len(items)):
     if x == current:
       if len(title_text) > 0:
-        print("\t[*] %s" % list(items.keys())[x])
+        print("%s\t> %s" % (colors[current_color], list(items.keys())[x]))
       else:
-        print("[*] %s" % list(items.keys())[x])
+        print("%s> %s" % (colors[current_color], list(items.keys())[x]) + colors["white"])
     else:
         if len(title_text) > 0:
-          print("\t[ ] %s" % list(items.keys())[x])
+          print("%s\t %s" % (colors["white"], list(items.keys())[x]))
         else:
-          print("[ ] %s" % list(items.keys())[x])
+          print("%s %s" % (colors["white"], list(items.keys())[x]))
 
 def render():
-  global current
+  global current, current_color
   replit.clear()
   populate()
   while True:    
@@ -50,6 +69,7 @@ def render():
           replit.clear()
           populate()
       elif key == 67:
+        print(colors["white"])
         separator()
         print("\t\toutput")
         separator()
