7706ba721834ca9705cd1afc4046844280e16bb4
Gabe <16624137+pain@users.noreply.github.com>
2019-03-20 22:17:42 -0400

    cmd updated

--- lib/inertia.py
+++ lib/inertia.py
@@ -1,49 +1,82 @@
+from getch import getch
 import os
-from msvcrt import getch
-from subprocess import call
 
 items = {}
+current = 0
+title_text = ""
+clear = lambda: os.system('cls')
 
+current_color = ""
+colors = {
+  "black": "\033[1;30;1m",
+  "red": "\033[1;31;1m",
+  "green": "\033[1;32;1m",
+  "yellow": "\033[1;33;1m",
+  "blue": "\033[1;34;1m",
+  "purple": "\033[1;35;1m",
+  "cyan": "\033[1;36;1m",
+  "white": "\033[1;37;1m"
+}
 
 def separator():
-    print("-----------------------")
+  print("-----------------------")
 
+def color(col):
+  global current_color
+  current_color = col
 
-def clear():
-    _ = call('clear' if os.name =='posix' else 'cls')
-
+def title(text):
+  global title_text
+  title_text = text
 
 def add_item(name, function):
-    items[name] = function
+  items[name] = function
 
+if current_color == "":
+  current_color = "white"
 
-def display():
-    current = 0
-    while True:
-        for x in range(0, len(items)):
-            if x == current:
-                print("[*] %s" % list(items.keys())[x])
-            else:
-                print("[ ] %s" % list(items.keys())[x])
+def populate():
+  if len(title_text) > 0:
+    print(colors["white"] + title_text)
+  for x in range(0, len(items)):
+    if x == current:
+      if len(title_text) > 0:
+        print("%s\t> %s" % (colors[current_color], list(items.keys())[x]))
+      else:
+        print("%s> %s" % (colors[current_color], list(items.keys())[x]) + colors["white"])
+    else:
+        if len(title_text) > 0:
+          print("%s\t %s" % (colors["white"], list(items.keys())[x]))
+        else:
+          print("%s %s" % (colors["white"], list(items.keys())[x]))
 
-        key = ord(getch())
-        if key == 224:  # special keys
-            key = ord(getch())
-            if key == 80: # down arrow
-                current = current + 1
-                print(current)
-            elif key == 72: # up arrow
-                if current > 0:
-                    current = current - 1
-                else:
-                    current = 0
-                print(current)
-            elif key == 77: # right arrow
-              if callable(list(items.values())[current]):
-                  list(items.values())[current]()
-              else:
-                  eval(list(items.values())[current])
-            '''
-            elif key == 75: # left arrow
-                print('left')
-            '''
+def render():
+  global current, current_color
+  clear()
+  populate()
+  while True:    
+      key = ord(getch())
+      if key == 80: # down arrow
+          if current <= 0:
+            current = current + 1
+          else:
+            current = len(items) - 1
+          clear()
+          populate()
+      elif key == 72: # up arrow
+          if current > 0:
+              current = current - 1
+          else:
+              current = 0
+          clear()
+          populate()
+      elif key == 77: # right arrow
+        clear()
+        print(colors["white"], end = "")
+        if callable(list(items.values())[current]):
+            list(items.values())[current]()
+        else:
+            eval(list(items.values())[current])
+      elif key == 75 or key == 27 or key == 13: # back arrow, escape, enter
+        clear()
+        populate()
