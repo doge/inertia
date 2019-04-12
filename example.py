1550f89e115a2f7749807e6c5cff3ac39d3bbfa8
Gabe <16624137+pain@users.noreply.github.com>
2019-04-12 10:29:46 -0400

    update example.py

--- example.py
+++ example.py
@@ -1,15 +1,22 @@
 import inertia
 
-def hello():
-    print("Hello")
+'''
+  colors:
+    black, red, green, yellow, blue, purple, cyan, white
+'''
 
-def world():
-    print("World")
+def hello_world():
+  print("hello world")
 
-inertia.title("My Menu")
-inertia.color("red")
-inertia.indicator(">")
-inertia.add_item("hello", hello)
-inertia.add_item("world", world)
-inertia.add_item("hello world", 'print("hello world")')
-inertia.render()
+def world_hello():
+  print("world hello")
+
+main = inertia.main_menu("this is our first menu title", "red", ">") # (self, title, color, indicator)
+main.add_item("hello world", hello_world)
+
+sub = inertia.sub_menu("this is our second menu title", "blue", ">")
+sub.add_item("hello world | sub (1)", hello_world)
+sub.add_item("hello world | sub (2)", world_hello)
+main.add_sub("sub-menu", sub)
+
+main.render()
